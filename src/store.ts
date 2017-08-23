00f3c25c9ad443a0398084adc4649024522443fb
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-08-23 15:42:30 +0200

    update repatch@1.2.0

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,10 @@
-import Store, { thunk, Reducer, Thunk } from 'repatch'
-import { State, Todo, TodoThunk } from './types'
-import api from './api'
+import Store, { thunk } from 'repatch';
+import { State, Todo } from './types';
+import api from './api';
 
 export default new Store<State>({
   todos: [],
   isFetching: false,
   error: null,
   removingTodoId: null
-}).addMiddleware<TodoThunk<any>>(thunk.withExtraArgument(api))
+}).addMiddleware(thunk.withExtraArgument(api));
